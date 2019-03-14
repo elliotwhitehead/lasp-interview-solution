6ad1b4b4f44357b9318f01e554e423e70b16f465
Elliot Whitehead <elliot.whitehead@colorado.edu>
2019-03-14 07:31:19 -0600

    Add Courage, Stupidity decoding

--- kerbals.py
+++ kerbals.py
@@ -1,4 +1,9 @@
-import csv, json
+import csv, json, math
+
+#Optional: Decode transformed stupidity / courage values
+def un_transform(data):
+	num = (float)(data)
+	return round((math.atan(num)+(math.pi/2))*(100/math.pi))
 
 #open csv as a file object, create csvreader
 with open('kerbals.csv', newline='') as kerbalscsv:
@@ -11,7 +16,11 @@ with open('kerbals.csv', newline='') as kerbalscsv:
 	for kerbal_data in kerbonauts:
 		new_kerbal = {}
 		for i in range(len(headers)):
-			new_kerbal[headers[i]] = kerbal_data[i]
+			#translate courage and stupidity to human-friendly values (percentage)
+			if(headers[i] == "Courage" or headers[i] == "Stupidity"):
+				new_kerbal[headers[i]] = un_transform(kerbal_data[i])
+			else:
+				new_kerbal[headers[i]] = kerbal_data[i]
 		roster.append(new_kerbal)
 
 	#convert to json array
